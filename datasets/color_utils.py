5d4790d3658413ebd5a92b41f009d4b89873171a
Wenri <Wenri@users.noreply.github.com>
2022-10-20 00:10:40 +0800

    conver color space of input image to sRGB

--- datasets/color_utils.py
+++ datasets/color_utils.py
@@ -1,9 +1,13 @@
-import cv2
+import logging
+from io import BytesIO
+
 import torch
+import torchvision
+from PIL import Image, ImageCms
 from einops import rearrange
-from imageio import imread
 import numpy as np
 from kornia.color import rgb_to_yuv
+import torchvision.transforms.functional as F
 
 
 def srgb_to_linear(img):
@@ -19,17 +23,32 @@ def linear_to_srgb(img):
 
 
 def read_image(img_path, img_wh, blend_a=True):
-    img = imread(img_path).astype(np.float32) / 255.0
-    # img[..., :3] = srgb_to_linear(img[..., :3])
-    if img.shape[2] == 4:  # blend A to RGB
-        if blend_a:
-            img = img[..., :3] * img[..., -1:] + (1 - img[..., -1:])
-        else:
-            img = img[..., :3] * img[..., -1:]
+    log = logging.getLogger(__name__)
+    # Read image
+    with Image.open(img_path) as image:
+        orig_icc = image.info.get('icc_profile')
+        # Extract original ICC profile
+        with BytesIO(orig_icc) as icc:
+            orig_icc = ImageCms.ImageCmsProfile(icc)
+        desc = ImageCms.getProfileDescription(orig_icc)
 
-    img = cv2.resize(img, img_wh)
-    img = rearrange(torch.as_tensor(img), 'h w c -> 1 c h w')
-    img = rgb_to_yuv(img)
-    img = rearrange(img.squeeze(0), 'c h w -> (h w) c')
+        # Plot image with original ICC profile
+        log.debug('Original ICC profile: {}'.format(desc))
+
+        # Create sRGB ICC profile and convert image to sRGB
+        srgb_icc = ImageCms.createProfile('sRGB')
+        image = ImageCms.profileToProfile(image, orig_icc, srgb_icc)
+
+    w, h = img_wh
+    img = F.resize(image, size=[h, w], interpolation=F.InterpolationMode.BICUBIC)
+    img = F.to_tensor(img)
+    if img.shape[0] == 4:  # blend A to RGB
+        if blend_a:
+            img = img[:3] * img[-1:] + (1 - img[-1:])
+        else:
+            img = img[:3] * img[-1:]
+
+    img = rgb_to_yuv(rearrange(img, 'c h w -> 1 c h w')).squeeze(0)
+    img = rearrange(img, 'c h w -> (h w) c')
 
     return img
